4cf1c698ef8e561fd8fb1e08878d0885efbab92a
Marc Giestin Louis Cordova <154757927+cordy001@users.noreply.github.com>
2025-05-17 21:25:23 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -37,22 +37,15 @@ echo " "
 
 echo " "
 echo "##############################"
-echo "Creating Dummy File"
+echo "Creating next environment"
 echo "##############################"
 echo " "
 
-echo " "
-echo "Dummy 1"
+mkdir nextjs
 
-touch /home/kali/.npm/_cacache/tmp/647ddeed
+cd nextjs
 
-echo "done."
-
-echo " "
-echo "Dummy 2"
-touch /home/kali/.npm/_cacache/content-v2/sha512/13/3c/b76081591e16d0c532e8e3d58a7c7026c2e6ad3770815e70ffccec10e95a8c8731efc46c13c2985226fe9cde905a24417ff35da7d562ff4bf020255532ab
-
-echo "done."
+npm init react-app@5.0.0 .
 
 echo "
 -------
